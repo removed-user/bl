27061a2b7def8f81a0c0e4cf04ff05ffc7d3b950
casey <caseyriley94@gmail.com>
2026-06-19 18:26:47 -0400

    init

---
+++ flakeModule.nix
@@ -0,0 +1,18 @@
+providerFlake: {lib, ...}: {
+  # _class = "generic";
+
+  options.flake = {
+  };
+  config = {
+    _module.args.Mylib = {
+      lib1 = import ./lib1.nix {inherit lib;}; # import a/the default flakeModule, and load lib from it
+      #or
+      lib2.define_monad = rec {
+        whatsAMonad = self: ''A Monad is just a monoid in the category of endofunctors'';
+        __functor = self: whatsAMonad;
+      };
+    };
+  };
+
+  # _module.args.Mylib
+}
